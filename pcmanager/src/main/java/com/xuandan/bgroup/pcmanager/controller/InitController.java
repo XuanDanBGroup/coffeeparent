bd7dffb5b4b7c074cae13820191c10adca5e91d4
BonePoints <704661673@qq.com>
2019-12-24 11:06:20 +0800

    login

---
+++ pcmanager/src/main/java/com/xuandan/bgroup/pcmanager/controller/InitController.java
@@ -0,0 +1,28 @@
+package com.xuandan.bgroup.pcmanager.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * @Description 初始化请求
+ * @Author 赵泽明
+ * @Version 1.0
+ */
+@Controller
+public class InitController {
+
+    @RequestMapping("/PCManager")
+    public String PCManger(){
+        return "loginManager";
+    }
+
+    @RequestMapping("/loginManager")
+    public String loginManger(String username,String password ){
+        if(username.equals("admin")&&password.equals("123456")){
+            return "";
+        }
+        return "";
+    }
+
+
+}
